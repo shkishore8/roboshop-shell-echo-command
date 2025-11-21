1db34d2ddf34ee97834f0f7c2cd295c449ee8d9f
kishore babu <shkishore8@gmail.com>
2025-11-21 09:44:43 +0530

    variable added

--- comman-function.sh
+++ comman-function.sh
@@ -2,3 +2,10 @@ sample1() {
 
   echo sample common funtion
 }
+
+sample() {
+
+  b=20
+  echo sample function
+  echo a -$a
+}
--- function.sh
+++ function.sh
@@ -1,14 +1,17 @@
 
 #Declaring the function
-
  sample () {
    echo sample function
 
  }
-
   #calling function
  sample
-
 #source common-function
  source comman-function.sh
  sample1
+
+
+a=10
+source comman-function.sh
+sample
+echo b -$b
