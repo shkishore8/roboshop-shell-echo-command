77ce5ad62b4ff8edaf6f9cd2b2a52fe3ae2c48e7
kishore babu <shkishore8@gmail.com>
2025-11-21 09:14:26 +0530

    variable added

---
+++ roboshop-variable.sh
@@ -0,0 +1,29 @@
+component=catalogue
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+cp catalogue.service /etc/systemd/system/${component}.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+rm -fr /app
+mkdir /app
+
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+cd /app
+unzip /tmp/${component}.zip
+
+cd /app
+npm install
+
+dnf install mongodb-mongosh -y
+mongosh --host 172.31.65.47 </app/db/master-data.js
+
+
+systemctl daemon-reload
+
+systemctl enable ${component}
+systemctl restart ${component}
