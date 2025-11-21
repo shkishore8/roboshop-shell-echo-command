27eeb0198c19705ccc56f1ed64f550d63123db61
kishore babu <shkishore8@gmail.com>
2025-11-21 10:31:29 +0530

    variable added

--- function/roboshop-common.sh
+++ function/roboshop-common.sh
@@ -1,44 +1,45 @@
 nodejs() {
-
-   echo -e \\e[35mDisable default NodeJs \\e[0m
+ color=\\e[35m
+ nocolor=\\e[0m
+   echo -e colorDisable default NodeJs nocolor
   dnf module disable nodejs -y
 
-  echo -e \\e[35mEnable nodeJS\\e[0m
+  echo -e colorEnable nodeJSnocolor
   dnf module enable nodejs:20 -y
 
-  echo -e \\e[35mInstall nodeJS\\e[0m
+  echo -e colorInstall nodeJSnocolor
   dnf install nodejs -y
 
-  echo -e \\e[35mAdd roboshop user\\e[0m
+  echo -e colorAdd roboshop usernocolor
   useradd roboshop
 
-  echo -e \\e[35mCopy nodejs config file\\e[0m
+  echo -e colorCopy nodejs config filenocolor
   cp ${component}.service /etc/systemd/system/${component}.service
 
-  echo -e \\e[35m copy mongodb repository\\e[0m
+  echo -e color copy mongodb repositorynocolor
   cp mongo.repo /etc/yum.repos.d/mongo.repo
   rm -fr /app
   mkdir /app
 
- echo -e \\e[35eDownload catalogue source code\\e[0m
+ echo -e \\e[35eDownload catalogue source codenocolor
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
   cd /app
 
-  echo \\e[35mUnzip catalogue source file\\e[0m
+  echo colorUnzip catalogue source filenocolor
   unzip /tmp/${component}.zip
 
- echo -e \\e[35m install NodeJS dependencies\\e[0m
+ echo -e colorinstall NodeJS dependenciesnocolor
   cd /app
   npm install
 
- echo -e \\e[35 Install mongodb DB\\e[0m
+ echo -e \\e[35 Install mongodb DBnocolor
   dnf install mongodb-mongosh -y
-  echo -e \\e[35mAdd mongodb schema\\[0m
+  echo -e colorAdd mongodb schema\\[0m
   mongosh --host 172.31.65.47 </app/db/master-data.js
 
 
   systemctl daemon-reload
- echo -e \\e[35mstart catalogue services\\e[0m
+ echo -e colorstart catalogue servicesnocolor
   systemctl enable ${component}
   systemctl restart ${component}
 }
